3d8d37e8bcbb54caa4098cb5149e6549ae44b05f
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-04-19 20:03:48 -0700

    tests

---
+++ split_test.go
@@ -0,0 +1,76 @@
+package nsid_test
+
+import (
+	"testing"
+
+	"github.com/reiver/go-nsid"
+)
+
+func TestSplit(t *testing.T) {
+
+	tests := []struct{
+		Value string
+		ExpectedDomainAuthority string
+		ExpectedName string
+	}{
+		{
+			Value:                   "",
+			ExpectedDomainAuthority: "",
+			ExpectedName:             "",
+		},
+
+
+
+		{
+			Value:                   "apple",
+			ExpectedDomainAuthority: "apple",
+			ExpectedName:                  "",
+		},
+		{
+			Value:                   "apple.banana",
+			ExpectedDomainAuthority: "apple.banana",
+			ExpectedName:                         "",
+		},
+		{
+			Value:                   "apple.banana.cherry",
+			ExpectedDomainAuthority: "apple.banana",
+			ExpectedName:                         "cherry",
+		},
+		{
+			Value:                   "apple.banana.cherry.date",
+			ExpectedDomainAuthority: "apple.banana.cherry",
+			ExpectedName:                                "date",
+		},
+	}
+
+	for testNumber, test := range tests {
+
+		actualDomainAuthority, actualName := nsid.Split(test.Value)
+
+		{
+			actual := actualDomainAuthority
+			expected := test.ExpectedDomainAuthority
+
+			if expected != actual {
+				t.Errorf("For test #%d, the actual 'domain-authority' is not what was expected.", testNumber)
+				t.Logf("EXPECTED: %q", expected)
+				t.Logf("ACTUAL:   %q", actual)
+				t.Logf("VALUE: %q", test.Value)
+				continue
+			}
+		}
+
+		{
+			actual := actualName
+			expected := test.ExpectedName
+
+			if expected != actual {
+				t.Errorf("For test #%d, the actual 'name' is not what was expected.", testNumber)
+				t.Logf("EXPECTED: %q", expected)
+				t.Logf("ACTUAL:   %q", actual)
+				t.Logf("VALUE: %q", test.Value)
+				continue
+			}
+		}
+	}
+}

6709d8242f1b035436b5e681e5440c177b3a3fe2
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-04-20 08:18:55 -0700

    docs

--- join.go
+++ join.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 )
 
-// Join combined an NSID domain-authority and an NSID name to construct an NSID.
+// Join combines an NSID domain-authority (such as "com.example") with an NSID name (such as "fooBar") to construct an NSID (such as "com.example.fooBar").
 //
 // Join more-or-less does the opposite of [Split].
 //
-// Join will normalize the domain-authority and also validate the overall NSID.
+// Join will normalize the domain-authority and will also validate the overall resulting NSID.
 // Join will return an error if the resulting NSID fails validation.
 func Join(domainAuthority string, name string) (string, error) {
 	str := fmt.Sprintf("%s.%s", domainAuthority, name)

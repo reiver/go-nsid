9fe5729178425943cc9c2f03735e06c22c26b59c
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-04-19 20:45:28 -0700

    nsid.NormalizeDomainAuthority()

--- normalizedomainauthority.go
+++ normalizedomainauthority.go
@@ -1,5 +1,28 @@
 package nsid
 
+// NormalizeDomainAuthority returns the normalized form of an domain-authority, as defined in:
+// https://atproto.com/specs/nsid
+//
+// The domain-authority is part of an NSID.
+//
+// For example, if the NSID was:
+//
+//	ONCE.TWICE.THRICE.FOURCE
+//
+// Then the domain-authority would be:
+//
+//	ONCE.TWICE.THRICE
+//
+// And then the normalized form of the domain-authority would be:
+//
+//	once.twice.thrice
+//
+// So, for example::
+//
+//	var domainAuthority string = "ONCE.TWICE.THRICE"
+//
+//	var result string = nsid.NormalizeDomainAuthority(domainAuthority)
+//	// result == "once.twice.thrice"
 func NormalizeDomainAuthority(value string) string {
 	const lenbuffer int = 256
 	var buffer [lenbuffer]byte

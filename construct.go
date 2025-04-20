004702420a9b3045a6fa77c2c837ef4a10ab9217
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-04-20 09:19:56 -0700

    docs

--- construct.go
+++ construct.go
@@ -4,12 +4,16 @@ import (
 	"strings"
 )
 
-// Construct creates an NSID (Namespaced Identifier) that is part of BlueSky's AT-Protocol, as defined here:
+// Construct creates an NSID (Namespaced Identifier).
+//
+// The NSID (Namespaced Identifier) is part of BlueSky's AT-Protocol, as defined here:
 // https://atproto.com/specs/nsid
 //
 // Construct is similar to [Join], but is more flexible.
 //
 // Construct allows the domain-authority to be broken into parts.
+//
+// Note that the NSID that Construct returns is normalized.
 func Construct(values ...string) string {
 	var nsID string = strings.Join(values, ".")
 	return Normalize(nsID)
